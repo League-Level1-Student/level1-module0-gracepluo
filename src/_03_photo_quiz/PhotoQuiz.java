e6ab0c56cbe0059ef9ec1ec257479bd826a237e5
League Student <league@iMac-14.attlocal.net>
2020-02-22 17:27:33 -0800

    Update PhotoQuiz.java
    I finished majority of #3

--- src/_03_photo_quiz/PhotoQuiz.java
+++ src/_03_photo_quiz/PhotoQuiz.java
@@ -26,27 +26,31 @@ public class PhotoQuiz {
 		// 1. find an image on the internet, and put its URL in a String
 		// variable (from your browser, right click on the image, and select
 		// “Copy Image Address”)
-String imageUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR92J31Qy28Ip7d6wmpHTixDZAkkqSAqYbp3f-K2vuUOE-nfESZ";
+String imageUrl = "https://www.nps.gov/chis/learn/nature/images/960-0M9B4594_1.jpg?maxwidth=650&autorotate=false";
 		// 2. create a variable of type "Component" that will hold your image
 Component c = createImage(imageUrl);
 		// 3. use the "createImage()" method below to initialize your Component
 
 		// 4. add the image to the quiz window
-
+quizWindow.add(c);
 		// 5. call the pack() method on the quiz window
-
+quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+String input = JOptionPane.showInputDialog("what is this bird");
 		// 7. print "CORRECT" if the user gave the right answer
-
+if(input.contentEquals("Raven")) {
+	System.out.println("Correct");
+}
 		// 8. print "INCORRECT" if the answer is wrong
-
+else {
+	System.out.println("No. This is raven");
+}
 		// 9. remove the component from the quiz window (you may not see the
 		// effect of this until step 12)
-
+quizWindow.remove(c);
 		// 10. find another image and create it (might take more than one line
 		// of code)
-
+Component c2 = 
 		// 11. add the second image to the quiz window
 
 		// 12. pack the quiz window
