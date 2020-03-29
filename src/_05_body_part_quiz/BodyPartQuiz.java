dfcb762abd10d260630d58e7edf2fd5b100b0a86
Grace <33816722+gracepluo@users.noreply.github.com>
2020-03-28 17:32:00 -0700

    worked on bodypart quiz

--- src/_05_body_part_quiz/BodyPartQuiz.java
+++ src/_05_body_part_quiz/BodyPartQuiz.java
@@ -42,7 +42,7 @@ public class BodyPartQuiz {
 
 		// 3. Change the size of the window so that you can only see part of the
 		// image.
-		window.setSize(500, 500);
+		window.setSize(100, 100);
 
 		showNextImage();
 
@@ -51,7 +51,7 @@ public class BodyPartQuiz {
 	private void startQuiz() {
 
 		// 1. Make an int variable to hold the score.
-
+		int score = 0;
 		// 2. Set the size of the window in the initializeGui() method 
 
 		// 4. Ask the user who this person is and store their answer
@@ -59,7 +59,14 @@ public class BodyPartQuiz {
 
 		// 5. Check their answer. If they guessed correctly:
 		// -- Tell them they are right and increase the score by 1
-
+		if (guess == ("Arnold")){
+			score++;
+			System.out.println("right");
+		}
+		else {
+		System.out.println("wrong, Arnold.");
+		}
+		System.out.println(score);
 		// 6. Otherwise:
 		// -- Tell them they are wrong and who the person is
 
@@ -67,7 +74,15 @@ public class BodyPartQuiz {
 		showNextImage();
 
 		// 8. .... repeat 4-7 for all your images.....
-
+		String guess1 = JOptionPane.showInputDialog("who is this?");
+		if (guess1 == ("Leo")){
+			score++;
+			System.out.println("right");
+		}
+		else {
+		System.out.println("wrong, Leo.");
+		}
+		System.out.println(score);
 		// 9. Show them their current score
 
 	}
