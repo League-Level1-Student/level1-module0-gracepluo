2cdea7c546b1a64fe6a75d23d5a8f1e8c6484c89
Grace <33816722+gracepluo@users.noreply.github.com>
2020-05-02 17:31:44 -0700

    worked on 1.7

--- src/_07_fortune_cookie/FortuneCookie.java
+++ src/_07_fortune_cookie/FortuneCookie.java
@@ -1,7 +1,28 @@
 package _07_fortune_cookie;
 
-public class FortuneCookie {
-    public void showButton() {
-        System.out.println("Button clicked");
-   }
-}
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class FortuneCookie implements ActionListener{
+    
+                  public void showButton() {
+            	JFrame frame = new JFrame();
+            	  JButton button = new JButton();
+            	    frame.add(button);
+            	    frame.pack();
+            	    button.addActionListener(this);
+            	    int rand = new Random().nextInt(5);
+            	    frame.setVisible(true);
+                System.out.println("Button clicked");
+           }
+
+				@Override
+				public void actionPerformed(ActionEvent arg0) {
+					
+				}
+        
+  }
---
+++ src/_07_fortune_cookie/Fortune_cookie_runner.java
@@ -0,0 +1,11 @@
+package _07_fortune_cookie;
+
+
+public class Fortune_cookie_runner {
+
+		public static void main(String[] args) {
+			new FortuneCookie().showButton();
+		}
+	}
+
+
