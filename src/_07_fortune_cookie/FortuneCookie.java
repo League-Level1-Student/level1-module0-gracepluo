72a401c81fb02bc8506b493dc24122fcb11e7bf1
Grace <33816722+gracepluo@users.noreply.github.com>
2020-05-09 17:27:15 -0700

    i think it's done.

--- src/_07_fortune_cookie/FortuneCookie.java
+++ src/_07_fortune_cookie/FortuneCookie.java
@@ -2,6 +2,7 @@ package _07_fortune_cookie;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.geom.Area;
 import java.util.Random;
 
 import javax.swing.JButton;
@@ -17,8 +18,24 @@ public class FortuneCookie implements ActionListener{
             	    button.addActionListener(this);
             	    int rand = new Random().nextInt(5);
             	    frame.setVisible(true);
+            	    if button.
                 System.out.println("Button clicked");
+                if (rand == 1){
+            System.out.println("Fortune one");
            }
+                if (rand == 2){
+            System.out.println("Fortune 2");
+           }
+                if (rand == 3){
+            System.out.println("Fortune three");
+           }
+                if (rand == 4){
+            System.out.println("Fortune four");
+           }
+                if (rand == 5){
+            System.out.println("Fortune five");
+           }
+                  }
 
 				@Override
 				public void actionPerformed(ActionEvent arg0) {
