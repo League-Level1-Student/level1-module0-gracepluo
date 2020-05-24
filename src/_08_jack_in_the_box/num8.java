e2f97c07f996cfe0fc09b0720305d7cb756644f8
Grace <33816722+gracepluo@users.noreply.github.com>
2020-05-23 17:56:18 -0700

    worked on part 8

--- src/_08_jack_in_the_box/num8.java
+++ src/_08_jack_in_the_box/num8.java
@@ -1,45 +1,50 @@
 package _08_jack_in_the_box;
 
 import java.applet.AudioClip;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.URL;
+import java.util.Random;
 
-import javax.print.DocFlavor.URL;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JApplet;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-public class num8 {
-
+public class num8 implements ActionListener {
 	private void showPicture(String fileName) { 
-	    try {
-	         JLabel imageLabel = createLabelImage(fileName);
-	         JFrame frame = new JFrame();
-	         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	         frame.add(imageLabel);
-	         frame.setVisible(true);
-	         frame.pack();
-	    } catch (Exception e) {
-	         e.printStackTrace();
-	    }
-	}
+	      try {
+	           JLabel imageLabel = createLabelImage(fileName);
+	           JFrame frame = new JFrame();
+	           frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	           frame.add(imageLabel);
+	           frame.setVisible(true);
+	           frame.pack();
+	      } catch (Exception e) {
+	           e.printStackTrace();
+	      }
+	 }
 
 
 
 	private JLabel createLabelImage(String fileName) {
-	   try {
+	     try {
 		          URL imageURL = getClass().getResource(fileName);
-	        if (imageURL == null) {
-	             System.err.println("Could not find image " + fileName);
-	             return new JLabel();
-	         } else {
-	              Icon icon = new ImageIcon(imageURL);
-	              JLabel imageLabel = new JLabel(icon);
-	              return imageLabel;
-	        }
-	    } catch (Exception e) {
-	         System.err.println("Could not find image " + fileName);
-	         return new JLabel();
-	   }
-	}
-	
+	          if (imageURL == null) {
+	               System.err.println("Could not find image " + fileName);
+	               return new JLabel();
+	           } else {
+	                Icon icon = new ImageIcon(imageURL);
+	                JLabel imageLabel = new JLabel(icon);
+	                return imageLabel;
+	          }
+	      } catch (Exception e) {
+	           System.err.println("Could not find image " + fileName);
+	           return new JLabel();
+	     }
+	 }
 	private void playSound(String soundFile) { 
 	      try {
 	           AudioClip sound = JApplet.newAudioClip(getClass().getResource(soundFile));
@@ -50,5 +55,21 @@ public class num8 {
 	 }
 
 
-	
+	 public void showButton() {
+	JFrame frame = new JFrame();
+	  JButton button = new JButton();
+	    frame.add(button);
+	    frame.pack();
+	    button.addActionListener(this);
+	  	    frame.setVisible(true);
+
+
+
+	 }
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}	
 }
---
+++ src/_08_jack_in_the_box/runner.java
@@ -0,0 +1,9 @@
+package _08_jack_in_the_box;
+
+public class runner {
+	public static void main(String[] args) {
+		new num8().showButton();
+	}
+}
+
+
