77db91430ed11917b226a297db962bb1249d780a
AAA1 <AAA1@AAA>
2020-06-20 17:25:09 -0700

    worked on and possibly finished 9

--- src/_09_sound_effects_machine/num9.java
+++ src/_09_sound_effects_machine/num9.java
@@ -1,5 +1,41 @@
 package _09_sound_effects_machine;
 
-public class num9 {
+import java.applet.AudioClip;
 
+import javax.swing.JApplet;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class num9 {
+	
+JButton button = new JButton();
+JFrame frame = new JFrame();
+JLabel alpha = new JLabel();
+int count = 0;
+	public static void main(String[] args) {
+		System.out.println();
+		
+		
+		
+		
+	}
+
+
+public void showButton() {
+// TODO Auto-generated method stub
+playSound("sawing-wood-daniel_simon.wav");
+}
+
+
+
+
+private void playSound(String fileName) {
+     AudioClip sound = JApplet.newAudioClip(getClass().getResource(fileName));
+     sound.play();
+
+}
+
+
+	
 }
